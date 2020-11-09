5275be87fd4b8d2bf2dba06a557b37294dc775e3
Hokuto Yamada <hokuto.yamada.j@gmail.com>
2020-11-09 15:29:13 +0900

    phone_number_push api

--- main.py
+++ main.py
@@ -10,15 +10,20 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 import os
+import json
 
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
-
+print('---------------------------------------- app started')
+app.logger.info('---------------------------------------- app started')
 
 @app.route("/callback", methods=['POST', 'GET'])
 def callback():
+    print('---------------------------------------- callback')
+    app.logger.info('---------------------------------------- callback')
+
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
@@ -35,9 +40,32 @@ def callback():
 
     return 'OK'
 
+
+@app.route("/phone_number_push", methods=['GET'])
+def phone_number_push(request):
+    to = '9e483b827f1b0b2e4b627b03ffdffa0da910ba4d3e21fa9f85dffa759afcc929'
+    messages = TextSendMessage(text='pnp test')
+    send_phone_number_push(to, messages)
+
+def send_phone_number_push(to, messages, delivery_tag=None, notification_disabled=False, timeout=None):
+    if not isinstance(messages, (list, tuple)):
+        messages = [messages]
+
+    if delivery_tag:
+        self.headers['X-Line-Delivery-Tag'] = delivery_tag
+
+    data = {
+        'to': to,
+        'messages': [message.as_json_dict() for message in messages],
+    }
+    line_bot_api._post(
+        '/bot/pnp/push', data=json.dumps(data), timeout=timeout
+    )
+
+
 @app.route("/", methods=['GET'])
 def index():
-    return hello_world(request)
+    return hello_world(None)
 
 def hello_world(request):
     to = 'h_yamada'
@@ -50,6 +78,7 @@ def hello_world(request):
     except Exception as e:
         print("------------ ERROR ------------")
         print(e)
+        print("------------ ERROR ------------")
         return 'ERROR'
 
 
